a3d2a203680ec68fe3752930779e24b3db90b1bb
贺镇涛 <1427113362@outlook.com>
2024-06-03 16:50:33 +0800

    Update

--- SECURITY.md
+++
@@ -1,3 +0,0 @@
-# Security
-
-If you find what you believe is a security issue with django-todo, please send a detailed report to django_todo_security_1213@birdhouse.org before publicizing. We thank you for your discretion.
--- todo/__init__.py
+++
@@ -1,9 +0,0 @@
-
-try:
-	# if django is not installed,
-	# skips check because it blocks automated installs
-	import django
-	from . import check
-except ModuleNotFoundError:
-	# this can happen during install time, if django is not installed yet!
-	pass
